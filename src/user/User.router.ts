c54963f3ccd810bc83d10f681a089b33ac7d893a
Sameer Niaz Mohammad <niazmohsameer@gmail.com>
2021-02-19 20:46:26 +0800

    Disable account creation

--- src/user/User.router.ts
+++ src/user/User.router.ts
@@ -1,6 +1,5 @@
 import {
   createHash,
-  createUser,
   getUserByHash,
   loginUser,
 } from "./User.service";
@@ -63,25 +62,29 @@ userRouter.post("/logout", (_, res) => {
   });
 });
 
-userRouter.post("/create", async (req, res) => {
-  try {
-    const username = req.body.username;
-    const password = req.body.password;
-    const user = await createUser(username, password);
-    const hash = await createHash(user.id.toString(), user.username);
-    res.status(201);
-    res.json({
-      success: true,
-      username: user.username,
-      hash: hash,
-    });
-  } catch (error) {
-    res.status(error.httpCode || 500);
-    res.json({
-      success: false,
-      message: error.message || "Internal server error",
-    });
-  }
+userRouter.post("/create", async (_, res) => {
+  // try {
+  //   const username = req.body.username;
+  //   const password = req.body.password;
+  //   const user = await createUser(username, password);
+  //   const hash = await createHash(user.id.toString(), user.username);
+  //   res.status(201);
+  //   res.json({
+  //     success: true,
+  //     username: user.username,
+  //     hash: hash,
+  //   });
+  // } catch (error) {
+  //   res.status(error.httpCode || 500);
+  //   res.json({
+  //     success: false,
+  //     message: error.message || "Internal server error",
+  //   });
+  // }
+  res.json({
+    success: false,
+    message: "Account creation is disabled for Seven Network during closed beta period.",
+  });
 });
 
 export default userRouter;
